aefe9f7d1ca8619f57668bee6212a9fb01e55b30
vci <vci@melexis.com>
2021-02-02 18:58:48 +0100

    adding webcontroller

--- lab04-web/src/main/java/junia/lab04/web/controller/CompanyController.java
+++ lab04-web/src/main/java/junia/lab04/web/controller/CompanyController.java
@@ -1,4 +1,22 @@
 package junia.lab04.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import junia.lab04.core.service.CompanyService;
+
+import javax.inject.Inject;
+
+@Controller
 public class CompanyController {
+    @Inject
+    CompanyService companyService ;
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String getListOfCompanies(ModelMap modelMap){
+        List<Company> companies = companyService.findAllWithProjects();
+        modelMap.addAllAttributes("companies",companies);
+        findAllWithProjects
+        return "companiesList";
+    }
 }
