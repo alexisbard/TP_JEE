2f34c569a988de099b4969ea5db3811d63f9d21b
vci <vci@melexis.com>
2021-02-03 18:09:11 +0100

    added the / form dans the delete option

--- lab04-web/src/main/java/junia/lab04/web/controller/CompanyController.java
+++ lab04-web/src/main/java/junia/lab04/web/controller/CompanyController.java
@@ -1,7 +1,10 @@
 package junia.lab04.web.controller;
 
+import jdk.swing.interop.SwingInterOpUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import junia.lab04.core.service.CompanyService;
@@ -19,5 +22,22 @@ public class CompanyController {
         List<Company> companies = companyService.findAllWithProjects();
         modelMap.addAttribute("companies",companies);
         return "companiesList";
+    }
+    @RequestMapping(value = "/form", method = RequestMethod.GET)
+    public String getForm(ModelMap modelMap){
+        Company company = new Company();
+        modelMap.put("company",company);
+        return "companyForm";
+    }
+
+    @RequestMapping(value = "/form", method = RequestMethod.POST)
+    public String submitForm(@ModelAttribute("company")Company  company){
+        companyService.save(company);
+        return "redirect:list";
+    }
+    @RequestMapping(value = "{id}/delete", method = RequestMethod.GET)
+    public String deleteCompany(@PathVariable("id") long id){
+        companyService.deleteById(id);
+        return "redirect:/list";
     }
 }
