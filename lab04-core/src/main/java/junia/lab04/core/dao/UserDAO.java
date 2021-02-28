b9e6aac012531352e00dc7bcb2712209e712ec85
vci <vci@melexis.com>
2021-02-28 22:53:55 +0100

    pour toa

--- lab04-core/src/main/java/junia/lab04/core/dao/UserDAO.java
+++ lab04-core/src/main/java/junia/lab04/core/dao/UserDAO.java
@@ -2,6 +2,12 @@ package junia.lab04.core.dao;
 
 import junia.lab04.core.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserDAO extends JpaRepository<User, Long> {
+    @Query("SELECT DISTINCT p FROM Product p ")
+    List<User> findAllUsers();
 }
+
